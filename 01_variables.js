af6d7b1fed10c49cc26a1013fcf44092f5a2aa92
srikanthreddykadapana <67358725+srikanthreddykadapana@users.noreply.github.com>
2023-08-21 13:05:38 +0000

    git commit done

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+//intentionally locked, the value cannot be changed
+const accountId = 906459
+let accountEmail = "Srikanth@google.com"
+var accountPassword = "123456"
+accountCity = "Hyderabad"
+
+
+accountEmail = "pari@joogle.com"
+accountPassword = "21212121"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
